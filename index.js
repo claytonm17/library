f5007f8f236ac65b591f2b1344d0a5b575842378
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-11-28 19:07:53 -0500

    feat: add function to remove book encapsulated functions within greatLibrary

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const myLibrary = [];
+const greatLibrary = (function(){
+    let myLibrary = [];
 
-class Book {
-    constructor(title, author, pages, isRead) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.isRead = isRead;
+    class Book {
+        constructor(
+            title = 'Unknown', 
+            author = 'Unknown',
+            pages = 0,
+            isRead = false
+        ) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.isRead = isRead;
+        }
     }
-}
-  
-function addBookToLibrary(title, author, pages, isRead) {
-    const newBook = new Book(title, author, pages, isRead);
-    myLibrary.push(newBook);
-}
+    
+    function addBookToLibrary(title, author, pages, isRead) {
+        const newBook = new Book(title, author, pages, isRead);
+        myLibrary.push(newBook);
+    }
 
-addBookToLibrary("Ishmael", "Daniel Quinn", 266, true);
+    function removeBook(title) {
+        myLibrary = myLibrary.filter(book => book.title !== title);
+    }
+
+    return {
+        addBookToLibrary,
+        removeBook,
+        getLibrary: () => myLibrary.slice()
+    }
+
+})();
+
+greatLibrary.addBookToLibrary("Ishmael", "Daniel Quinn", 266, true);
+greatLibrary.addBookToLibrary("Ishmael2", "Daniel Quinn2", 267, false);
